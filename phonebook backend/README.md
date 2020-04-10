7bd9a8810b5641155579b48595f868c56ad8ac0c
Ndhote Ilyas <ii.ndhote@gmail.com>
2020-04-11 00:31:03 +0300

    Build for the front-end

---
+++ phonebook backend/README.md
@@ -0,0 +1,1 @@
+[https://protected-caverns-46099.herokuapp.com/api/persons](https://protected-caverns-46099.herokuapp.com/api/persons)
---
+++ phonebook backend/build/precache-manifest.8f59448562eca5355a175c5f42ed5793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c58811005793c371de2875949fa87f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ece86efc133f689eab8",
+    "url": "/static/css/main.77b5518b.chunk.css"
+  },
+  {
+    "revision": "c8806c721a9ce1bb3bb5",
+    "url": "/static/js/2.1c4526c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c4526c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ece86efc133f689eab8",
+    "url": "/static/js/main.29e2581b.chunk.js"
+  },
+  {
+    "revision": "d19032c1263fa01ea41a",
+    "url": "/static/js/runtime-main.08633c47.js"
+  }
+]);
